a9d9bac5cfc2e4ec6e0d5c9cafa24bdd1f38c8b0
dheerajku <dheerajkushwah2015@gmail.com>
2019-12-08 03:11:05 +0530

    remove config/credentials.yml.enc file

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ set :application, "twitter"
 set :repo_url, "git@github.com:dheerajku/twitter_app.git"
 
 set :use_sudo, true
+set :rvm_ruby_version, 'ruby-2.5.1p57'
 # Deploy to the user's home directory
 set :deploy_to, "/home/ubuntu/#{fetch :application}"
 
@@ -13,3 +14,5 @@ set :keep_releases, 5
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
+set :npm_flags, '--production' # default
